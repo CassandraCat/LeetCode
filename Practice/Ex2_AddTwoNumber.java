c306d9a15469430fafa58d558e4f89a375822ae3
Peach Zhang <CrazyCatZhang@outlook.com>
2020-08-12 23:39:53 +0800

    Update Ex2_AddTwoNumber.java

--- Practice/Ex2_AddTwoNumber.java
+++ Practice/Ex2_AddTwoNumber.java
@@ -21,27 +21,50 @@ public class Ex2_AddTwoNumber {
         ListNode archive = new ListNode(0);
         ListNode temp = archive;
         boolean flag = false;
-        while(list1 != null || list2 != null){
+        while (list1 != null || list2 != null) {
             int values1 = list1 != null ? list1.val : 0;
             int values2 = list2 != null ? list2.val : 0;
             int sum = flag ? (values1 + values2 + 1) : (values1 + values2);
-            if(sum >= 10){
-                sum -= 10;
+            if (sum >= 10) {
+                sum %= 10;
                 flag = true;
-            }else{
+            } else {
                 flag = false;
             }
-            temp.next = new ListNode(sum % 10);
+            temp.next = new ListNode(sum);
             temp = temp.next;
-            if(list1 !=null){
+            if (list1 != null) {
                 list1 = list1.next;
             }
-            if(list2 != null){
+            if (list2 != null) {
                 list2 = list2.next;
             }
         }
-        if(flag){
+        if (flag) {
             temp.next = new ListNode(1);
+        }
+        return archive.next;
+    }
+
+    public static ListNode addTwoNumbers2(ListNode listNode1, ListNode listNode2) {
+        ListNode list1 = listNode1;
+        ListNode list2 = listNode2;
+        ListNode archive = new ListNode(0);
+        ListNode temp = archive;
+        int current = 0;
+        while (list1 != null || list2 != null || current != 0) {
+            int values1 = list1 != null ? list1.val : 0;
+            int values2 = list2 != null ? list2.val : 0;
+            int sum = values1 + values2 + current;
+            current = sum / 10;
+            temp.next = new ListNode(sum % 10);
+            temp = temp.next;
+            if (list1 != null) {
+                list1 = list1.next;
+            }
+            if (list2 != null) {
+                list2 = list2.next;
+            }
         }
         return archive.next;
     }
