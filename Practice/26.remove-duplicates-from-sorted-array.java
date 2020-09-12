64b62eb753b37df02c5bbdf3a7c6817507de6e7f
Peach Zhang <CrazyCatZhang@outlook.com>
2020-09-12 23:36:28 +0800

    Update 26.remove-duplicates-from-sorted-array.java

--- Practice/26.remove-duplicates-from-sorted-array.java
+++ Practice/26.remove-duplicates-from-sorted-array.java
@@ -7,7 +7,13 @@
 // @lc code=start
 class Solution {
     public int removeDuplicates(int[] nums) {
-        
+        int i = nums.length > 0 ? 1 : 0;
+        for (int n : nums) {
+            if (n > nums[i - 1]) {
+                nums[i++] = n;
+            }
+        }
+        return i;
     }
 }
 // @lc code=end
